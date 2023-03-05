e515d4d8124ea93e345ad6dec887e2b962c9a240
Natan <natandeveloper@icloud.com>
2023-03-05 19:48:04 +0000

    Initial Commit

---
+++ AnotherAttemptOnVIPER/AnotherAttemptOnVIPERApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherAttemptOnVIPERApp.swift
+//  AnotherAttemptOnVIPER
+//
+//  Created by Natan Gluszko on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnotherAttemptOnVIPERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
